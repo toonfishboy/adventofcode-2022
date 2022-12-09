90a6522ed30c1313619c775265a4add56be47b50
Eric Stampa <eric.stampa@cgi.com>
2022-12-09 09:28:32 +0100

    Finished part 1 of Day09

---
+++ src/09/task.ts
@@ -0,0 +1,99 @@
+import {readLines} from "../helper";
+import {join} from "path";
+
+export async function runTask1() {
+    const lines = await readLines(join(__dirname, "input.txt"));
+    let hX = 0, hY = 0, tX = 0, tY = 0;
+    let visited: string[] = ["0:0"];
+    const getTPos = () => `${tX}:${tY}`;
+    lines.forEach(line => {
+        const [direction, count] = line.split(" ");
+        for (let i = 0; i < parseInt(count); i++) {
+            switch (direction) {
+                case "R":
+                    hX++;
+                    break;
+                case "U":
+                    hY--;
+                    break;
+                case "L":
+                    hX--;
+                    break;
+                case "D":
+                    hY++;
+                    break;
+            }
+            if (Math.abs(hX - tX) === 2 || Math.abs(hY - tY) === 2) {
+                if (Math.abs(hX - tX) === 2) {
+                    tX += hX > tX ? 1 : -1;
+                    tY = hY;
+                }
+                if (Math.abs(hY - tY) === 2) {
+                    tY += hY > tY ? 1 : -1;
+                    tX = hX;
+                }
+                if (!visited.includes(getTPos()))
+                    visited.push(getTPos());
+            }
+        }
+    });
+    console.log(visited.length);
+}
+
+type Pos = {
+    x: number;
+    y: number;
+}
+
+export async function runTask2() {
+    const lines = await readLines(join(__dirname, "example.txt"));
+    const knots: Pos[] = new Array(10).fill(0).map(_ => ({x: 0, y: 0}));
+    const getTPos = (knot: Pos) => `${knot.x}:${knot.y}`;
+    const simulateKnot = (knot1: Pos, knot2: Pos, direction: string, visited: string[]) => {
+            switch (direction) {
+                case "R":
+                    knot2.x++;
+                    break;
+                case "U":
+                    knot2.y--;
+                    break;
+                case "L":
+                    knot2.x--;
+                    break;
+                case "D":
+                    knot2.y++;
+                    break;
+            }
+            if (Math.abs(knot2.x - knot1.x) === 2 || Math.abs(knot2.y - knot1.y) === 2) {
+                if (Math.abs(knot2.x - knot1.x) === 2) {
+                    knot1.x += knot2.x > knot1.x ? 1 : -1;
+                    knot1.y = knot2.y;
+                }
+                if (Math.abs(knot2.y - knot1.y) === 2) {
+                    knot1.y += knot2.y > knot1.y ? 1 : -1;
+                    knot1.x = knot2.x;
+                }
+                if (!visited.includes(getTPos(knot1)))
+                    visited.push(getTPos(knot1));
+            }
+    };
+
+    let visited: string[] = ["0:0"];
+    lines.forEach(line => {
+        for (let i = knots.length - 1; i >= 0; i--) {
+            if(i === 0) break;
+            const knot1 = knots[i - 1];
+            const knot2 = knots[i];
+            if(i === 1){
+                console.log(knot1, knot2);
+            }
+
+            const [direction, count] = line.split(" ");
+            for (let j = 0; j < parseInt(count); j++) {
+                simulateKnot(knot1, knot2, direction, i === 1 ? visited : []);
+            }
+        }
+    });
+    console.log(visited);
+    console.log(visited.length);
+}
